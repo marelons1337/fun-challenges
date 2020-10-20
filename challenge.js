5e1823b7265aaa179d3ee0dd0ee102446e64762b
Marelons1337 <jarorafo@gmail.com>
2020-10-20 22:26:59 +0200

    rps initial scripts

--- challenge.js
+++ challenge.js
@@ -4,6 +4,10 @@ const ageInput = document.getElementById("age-input-box");
 const resultBox = document.getElementById("age-result");
 const catButton = document.getElementById("cats-button")
 const catsContainer = document.getElementById("cats-container")
+const rock = document.getElementById("rock-image")
+const paper = document.getElementById("paper-image")
+const scissors = document.getElementById("scissors-image")
+const rpsResultBox = document.getElementById("rps-score")
 // Date input
 
 var input;
@@ -40,9 +44,85 @@ const generateCat = () => {
     })
 }
 
+const startGame = (e) => {
+    paper.style.display = "none";
+    rpsResultBox.style.display ="inline"
+    const winningClick = () => {
+        rpsResultBox.innerHTML = "You Win!";
+        rpsResultBox.className = '';
+        rpsResultBox.className = "winner";
+    }
+    const losingClick = () => {
+        rpsResultBox.innerHTML= "You Lose!";
+        rpsResultBox.className = '';
+        rpsResultBox.className = "loser";
+    }
+    const tiedClick = () => {
+        rpsResultBox.innerHTML = "You tied!";
+        rpsResultBox.className = '';
+        rpsResultBox.className = "tied";
+    }
+    const scoreRandomizer = () => {
+        const randomScore = getRndInteger(1,3);
+        switch (randomScore) {
+            case 1:
+                return "rock";
+                break;
+            case 2:
+                return "paper";
+                break;
+            case 3:
+                return "scissors";
+                break;
+        
+            default:
+                console.log("error");
+                break;
+        }
+    }
+    const score = scoreRandomizer();
+    console.log(score);
+    if(e.target.matches("#rock-image")){
+        console.log("rock");
+        if (score=="rock"){
+            console.log("you tied");
+            tiedClick();
+        }
+        else if (score=="paper"){
+            console.log("you lost");
+            losingClick();
+        }
+        else {
+            console.log("you win");
+            winningClick();
+        }
+    }
+    else if (e.target.matches("#scissors-image")){
+        console.log("scissors");
+        if (score=="rock")
+            console.log("you lost");
+        else if (score=="paper")
+            console.log("you win");
+        else console.log("you tied");
+    }
+    else if(e.target.matches("#paper-image")){
+        console.log("paper");
+        if (score=="rock")
+            console.log("you win");
+        else if (score=="paper")
+            console.log("you tied");
+        else console.log("you lost");
+    }
+
+}
+
 ageInput.addEventListener("change", readAgeInput);
 ageButton.addEventListener("click", calculateAge);
 resetButton.addEventListener("click", resetAge);
-catButton.addEventListener("click", generateCat)
+catButton.addEventListener("click", generateCat);
+rock.addEventListener("click", startGame);
+paper.addEventListener("click", startGame);
+scissors.addEventListener("click", startGame);
+
 
 
